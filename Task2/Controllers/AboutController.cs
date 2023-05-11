87d258d0fa3b725c56414eb844c685b55cc5c211
unknown <shamsshsh@code.edu.az>
2023-05-11 21:51:50 +0400

    taskclass

---
+++ Task2/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task2.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
